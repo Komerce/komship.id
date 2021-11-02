89b129bc76e697ff2e243116838204e0e908c5bf
ricoerlanggaa <ricoerlanggaa@gmail.com>
2021-11-02 08:42:29 +0700

    fix responsive footer

--- src/components/footers/Default.vue
+++ src/components/footers/Default.vue
@@ -2,24 +2,24 @@
     <v-footer style="background-color:#F6F6F9;">
       <div class="container">
         <div class="col-md-12">
-          <div class="row mb-5">
-            <div class="profile col-md-6">
-              <v-img :src="require('@/assets/img/komship-logo.png')" width="200px" />
-              <div class="row mb-2">
-                <div class="col-sm-1"><v-img :src="require('@/assets/img/icons/location-icon.png')" /></div>
-                <div class="col-sm-6">Jl. Raya Tamansari, Kompleks Karangwuni, Desa Tamansari, Karangmoncol, Kab. Purbalingga, Jawa Tengah 53355</div>
+          <div class="row">
+            <div class="col-md-6 profile">
+              <v-img :src="require('@/assets/img/logo/komship-logo.png')" class="profile-image" />
+              <div class="row">
+                <div class="col-sm-1 profile-icon"><v-img :src="require('@/assets/img/icons/location-icon.png')" max-width="30px" min-width="25px" /></div>
+                <div class="col-sm-8 profile-desc">Jl. Raya Tamansari, Kompleks Karangwuni, Desa Tamansari, Karangmoncol, Kab. Purbalingga, Jawa Tengah 53355</div>
               </div>
-              <div class="row mb-2">
-                <div class="col-sm-1"><v-img :src="require('@/assets/img/icons/email-icon.png')" /></div>
-                <div class="col-sm-6">support@komship.id</div>
+              <div class="row">
+                <div class="col-sm-1 profile-icon"><v-img :src="require('@/assets/img/icons/email-icon.png')" max-width="30px" min-width="20px" /></div>
+                <div class="col-sm-8 profile-desc">support@komship.id</div>
               </div>
-              <div class="row mb-2">
-                <div class="col-sm-1"><v-img :src="require('@/assets/img/icons/call-icon.png')" /></div>
-                <div class="col-sm-6">081227931840</div>
+              <div class="row">
+                <div class="col-sm-1 profile-icon"><v-img :src="require('@/assets/img/icons/call-icon.png')" max-width="30px" min-width="20px" /></div>
+                <div class="col-sm-8 profile-desc">081227931840</div>
               </div>
             </div>
-            <div class="company col-md-3">
-              <h5>Perusahaan</h5>
+            <div class="col-md-3 company">
+              <h5 class="company-title">Perusahaan</h5>
               <ul class="company-link">
                 <li><router-link to="about">Tentang Kami</router-link></li>
                 <li><router-link to="terms">Syarat & Ketentuan</router-link></li>
@@ -27,17 +27,17 @@
                 <li><router-link to="faq">FAQ</router-link></li>
               </ul>
             </div>
-            <div class="partner col-md-3">
-              <h5>Kerjasama Ekspedisi</h5>
+            <div class="col-md-3 partner">
+              <h5 class="partner-title">Kerjasama Ekspedisi</h5>
               <div class="image-partner">
-                <v-img :src="require('@/assets/img/jne-logo.png')" max-width="100px" />
+                <v-img :src="require('@/assets/img/logo/jne-logo.png')" max-width="100px" />
               </div>
             </div>
-          </div>
-          <div class="row d-flex justify-content-end">
-            <v-img :src="require('@/assets/img/icons/whatsapp-icon.png')" max-width="50px" />
-            <v-img :src="require('@/assets/img/icons/instagram-icon.png')" max-width="50px" />
-            <v-img :src="require('@/assets/img/icons/facebook-icon.png')" max-width="50px" />
+            <div class="col-md-12 contact">
+              <v-img :src="require('@/assets/img/icons/whatsapp-icon.png')" class="ml-3" max-width="30px" />
+              <v-img :src="require('@/assets/img/icons/instagram-icon.png')" class="ml-3" max-width="30px" />
+              <v-img :src="require('@/assets/img/icons/facebook-icon.png')" class="ml-3" max-width="30px" />
+            </div>
           </div>
           <div class="copyright row d-flex">
             Copyright 2021 komship.id. All rights reserved.
@@ -50,9 +50,18 @@
 .profile{
   color: #828282;
 }
-.company h5{
+.profile .profile-image{
+  max-width: 200px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+.profile .row{
+  margin-bottom: 10px;
+}
+.company .company-title{
   font-weight: 600;
-  margin: 20px 0;
+  margin-top: 60px;
+  margin-bottom: 20px;
 }
 .company-link{
   list-style-type: none;
@@ -68,14 +77,85 @@
 .company-link a:hover{
   color: #FF6A3A;
 }
-.partner h5{
+.partner .partner-title{
   font-weight: 600;
-  margin-top: 20px;
+  margin-top: 60px;
 }
 .image-partner{
   margin-left: 3vw;
 }
+.contact{
+  padding-top: 50px;
+  display: flex;
+  justify-content: end;
+}
 .copyright{
   color: #828282;
+}
+@media screen and (max-width: 1024px) {
+  .profile{
+    width: 70%!important;
+  }
+  .profile-icon{
+    width: 10%!important;
+  }
+  .profile-desc{
+    width: 70%!important;
+  }
+  .company{
+    width: 30%!important;
+  }
+  .partner{
+    width: 70%!important;
+  }
+  .partner .partner-title{
+    margin-top: 30px;
+  }
+  .contact{
+    width: 30%!important;
+    justify-content: start;
+  }
+  .contact img{
+    padding-top: 0;
+    top: 0;
+  }
+  .copyright{
+    justify-content: center;
+    margin-top: 50px!important;
+  }
+}
+@media screen and (max-width: 720px) {
+  .profile{
+    width: 60%!important;
+  }
+  .company{
+    width: 30%!important;
+  }
+  .partner{
+    width: 60%!important;
+  }
+  .contact{
+    width: 40%!important;
+  }
+}
+@media screen and (max-width: 540px) {
+  .profile{
+    width: 100%!important;
+  }
+  .company{
+    width: 100%!important;
+  }
+  .company .company-title{
+    margin-top: 30px;
+  }
+  .partner{
+    width: 100%!important;
+  }
+  .partner .partner-title{
+    margin-top: 30px;
+  }
+  .contact{
+    width: 100%!important;
+  }
 }
 </style>
