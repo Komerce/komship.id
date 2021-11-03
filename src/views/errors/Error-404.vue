2505ebb81eaf09e8d1cdbbca1d985c46bda038bf
ricoerlanggaa <ricoerlanggaa@gmail.com>
2021-11-03 10:56:53 +0700

    create page error-404

--- src/views/errors/Error-404.vue
+++ src/views/errors/Error-404.vue
@@ -1,3 +1,61 @@
 <template>
-    <v-app><h1>Error</h1></v-app>
+    <v-app>
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <img :src="require('@/assets/img/logo/komship-logo.png')" class="image-navbar" />
+            </div>
+        </nav>
+        <section class="error-404">
+            <div class="container">
+                <v-img :src="require('@/assets/img/partials/404.png')" class="image-error" />
+                <h1 class="title-error">Whoops!</h1>
+                <p class="desc-error">Maaf, kami tidak bisa menemukan halaman yang kamu cari.</p>
+                <div class="backhome">
+                    <button class="btn-backhome">Kembali ke beranda</button>
+                </div>
+            </div>
+        </section>
+    </v-app>
 </template>
+<style>
+.image-navbar{
+    margin-left: 5vw;
+}
+.error-404{
+    background-color: #f8f9fa;
+    height: 100%;
+}
+.image-error{
+    margin: auto;
+    width: 300px;
+}
+.title-error{
+    font-weight: 700;
+    display: flex;
+    justify-content: center;
+    margin-top: 2vw;
+}
+.desc-error{
+    display: flex;
+    justify-content: center;
+    margin-top: 1.5vw;
+    color: #828282;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+}
+.backhome{
+    margin-top: 3.5vw;
+    display: flex;
+    justify-content: center;
+}
+.btn-backhome{
+    background-color: #F95031;
+    width: 222px;
+    height: 50px;
+    border-radius: 12px;
+    color: #FFFFFF;
+    font-weight: 600;
+    font-size: 16px;
+}
+</style>
