3d20c5c9a54bbbfa87dc149ee1da9d9d858b3bd8
ricoerlanggaa <ricoerlanggaa@gmail.com>
2021-11-05 16:03:07 +0700

    add scroll behavior to top router link

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    document.getElementById("app").scrollIntoView();
+  },
 });
 
 export default router;
