89a466d8bb2327725822e004bcdcf4e4c91f78ad
ricoerlanggaa <ricoerlanggaa@gmail.com>
2022-04-05 16:29:41 +0700

    Setting Axios Global

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createMetaManager } from "vue-meta";
+import axios from "axios";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+
 const app = createApp(App);
 app.use(router);
+app.config.globalProperties.$http = axios;
 app.use(createMetaManager());
 app.mount("#app");
