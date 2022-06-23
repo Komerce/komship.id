68fb3bb956d00ec3a21da57867457f0d749f0c58
DESKTOP-TJEQ29F\Komerce <islakhunnurdzaki24@gmail.com>
2022-06-23 10:03:56 +0700

    asdasdasd

--- src/views/pages/Home/Home.js
+++ src/views/pages/Home/Home.js
@@ -10,54 +10,67 @@ export default {
       title: "Komship - Platform Kirim Barang",
       meta: [
         {
+          vmid: "description",
           name: "description",
           content: "Komship merupakan platform layanan pengiriman yang bertujuan memudahkan kamu untuk mengirim paket yang didukung dengan metode COD atau Non-COD tanpa batas minimum pengiriman."
         },
         {
+          vmid: "keywords",
           name: "keywords",
           content: "komship"
         },
         {
+          vmid: "og-locale",
           property: "og:locale",
           content: "id"
         },
         {
+          vmid: "og-type",
           property: "og:type",
           content: "website"
         },
         {
+          vmid: "og-title",
           property: "og:title",
           content: "Komship - Platform Kirim Barang"
         },
         {
+          vmid: "og-description",
           property: "og:description",
           content: "Komship merupakan platform layanan pengiriman yang bertujuan memudahkan kamu untuk mengirim paket yang didukung dengan metode COD atau Non-COD tanpa batas minimum pengiriman."
         },
         {
+          vmid: "og-url",
           property: "og:url",
           content: "https://komship.id/"
         },
         {
+          vmid: "og-site_name",
           property: "og:site_name",
           content: "Komship"
         },
         {
+          vmid: "og-image",
           property: "og:image",
           content: "https://komship.id/img/komship.png"
         },
         {
+          vmid: "og-image-width",
           property: "og:image:width",
           content: "1280"
         },
         {
+          vmid: "og-image-height",
           property: "og:image:height",
           content: "720"
         },
         {
+          vmid: "og-see_also",
           property: "og:see_also",
           content: "https://www.instagram.com/komship/"
         },
         {
+          vmid: "og-see_also2",
           property: "og:see_also",
           content: "https://www.facebook.com/komship/"
         }
@@ -65,12 +78,14 @@ export default {
       ],
       link: [
         {
+          vmid: "canonical",
           rel: "canonical",
           href:"https://komship.id/"
         }
       ],
       script:[
         { 
+          vmid: "jsonId",
           type: 'application/ld+json', 
           json: {
             "@context":"https://schema.org/", 
